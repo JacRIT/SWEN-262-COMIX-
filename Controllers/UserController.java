69d43bce815f96cf787fe0c2edd0e344f5c6dc07
jbc9236 <jbc9236@rit.edu>
2023-03-22 19:03:42 -0400

    Adding UserController.java

---
+++ Controllers/UserController.java
@@ -0,0 +1,14 @@
+package Controllers;
+import Model.JavaObjects.User;
+
+public class UserController{
+
+    public void create (User newUser){
+
+    }
+
+    public User get(int id){
+        return null;
+    }
+
+}
