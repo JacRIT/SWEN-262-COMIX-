bea768725ad9fc198bb101240ff78d313b45b48b
jbc9236 <jbc9236@rit.edu>
2023-03-26 18:06:20 -0400

    Commented userController class

--- Controllers/UserController.java
+++ Controllers/UserController.java
@@ -4,10 +4,11 @@ import Model.JavaObjects.User;
 public class UserController{
 
     public void create (User newUser){
-
+        /*Creating a user into the database */
     }
 
     public User get(int id){
+        /*Get a user using the id */
         return null;
     }
 
