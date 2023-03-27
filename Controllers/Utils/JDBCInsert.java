6661854ab2046f36d31ade175475bbbc2ca8e453
J Russ <jtr03@sbcglobal.net>
2023-03-27 13:44:37 -0400

    import fix

--- Controllers/Utils/JDBCInsert.java
+++ Controllers/Utils/JDBCInsert.java
@@ -5,7 +5,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 
 public class JDBCInsert extends JDBC {
 
