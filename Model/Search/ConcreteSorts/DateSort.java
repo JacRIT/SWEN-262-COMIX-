cbfa5ab786f49c91a16eb175c4cfb6682f911f5c
J Russ <jtr03@sbcglobal.net>
2023-03-27 13:34:57 -0400

    sort algorithms stubbed out

---
+++ Model/Search/ConcreteSorts/DateSort.java
@@ -0,0 +1,13 @@
+package Model.Search.ConcreteSorts;
+
+import Model.Search.SortAlgorithm;
+
+public class DateSort implements SortAlgorithm {
+
+    @Override
+    public String sort() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sort'");
+    }
+    
+}
