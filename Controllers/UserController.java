d4869f662db6096c88823816b90e4822a11b25ef
SpazzStuff <36232417+SpazzStuff@users.noreply.github.com>
2023-03-28 19:31:33 -0400

    first pass of getByUsername() there, JDBCRead created

--- Controllers/UserController.java
+++ Controllers/UserController.java
@@ -1,10 +1,29 @@
 package Controllers;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import Controllers.Utils.JDBCInsert;
+import Controllers.Utils.JDBCRead;
 import Model.JavaObjects.User;
 
 public class UserController{
+    JDBCRead jdbcRead;
+    JDBCInsert jdbcInsert;
 
-    public void create (User newUser){
+    public UserController() {
+        this.jdbcRead = new JDBCRead();
+        this.jdbcInsert = new JDBCInsert();
+    }
 
+    public User create (User newUser){  // change to just take username?
+        // add a row to collection_info (SERIAL id, nickname)
+        // get the id of the new collection (how?)
+        // add a row to collection_reference (SERIAL id, collection_fk, copy_fk(?))   
+        // add a row to user_info (SERIAL id, collection_fk, last_name(?), first_name(?), username)
+        // get the id of the new user, put it into a User, return it
+        return null;
     }
 
     public User get(int id){
@@ -15,8 +34,18 @@ public class UserController{
         // get all user information where the username matches, we are having usernames be unique
         // put that information into a User instance and return it
 
+        // String sql = "SELECT id FROM user_info WHERE username = %s;";
+        // ArrayList<Object> var = new ArrayList<>();
+        // var.add(username);
+        // ResultSet rs = jdbcRead.executePreparedSQL(sql, var);
+        // try {
+        //     return new User(rs.getInt("id"), username);
+        // } catch (SQLException e) {
+        //     System.out.println(e);
+        //     return null;
+        // }
+
         // mock return data
         return new User(2, username);
     }
-
 }
