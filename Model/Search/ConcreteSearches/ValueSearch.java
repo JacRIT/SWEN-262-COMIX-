bb4f1edd7c31ba28a83ae787cd55f62eab44aebc
J Russ <jtr03@sbcglobal.net>
2023-04-03 17:02:41 -0400

    value search fix

--- Model/Search/ConcreteSearches/ValueSearch.java
+++ Model/Search/ConcreteSearches/ValueSearch.java
@@ -13,56 +13,89 @@ public class ValueSearch extends SearchAlgorithm {
 
         result.appendToSql(
             """
+            SELECT id FROM
                 (
-                    WITH RECURSIVE subcollections AS ( --find all collection_ids that specified user owns
-                        SELECT 
-                            id,
-                            collect_fk,
-                            subcollect_fk
-                        FROM
-                            subcollection_refrence
-                        WHERE 
-                            collect_fk = (
-                                SELECT 
-                                    collection_fk
-                                FROM
-                                    user_info
-                                WHERE 
-                                    user_info.id = ?
-                            )
-                        UNION
+                    (
+                        WITH RECURSIVE subcollections AS ( --find all collection_ids that specified user owns
                             SELECT 
-                                e.id,
-                                e.collect_fk,
-                                e.subcollect_fk
+                                id,
+                                collect_fk,
+                                subcollect_fk
                             FROM
-                                subcollection_refrence e
-                            INNER JOIN 
-                                subcollections s 
-                                    ON s.subcollect_fk = e.collect_fk
+                                subcollection_refrence
+                            WHERE 
+                                collect_fk = (
+                                    SELECT 
+                                        collection_fk
+                                    FROM
+                                        user_info
+                                    WHERE 
+                                        user_info.id = ?
+                                )
+                            UNION
+                                SELECT 
+                                    e.id,
+                                    e.collect_fk,
+                                    e.subcollect_fk
+                                FROM
+                                    subcollection_refrence e
+                                INNER JOIN 
+                                    subcollections s 
+                                        ON s.subcollect_fk = e.collect_fk
                     )
                         SELECT
-                            comic_ownership.id
+                            comic_ownership.id,
+                            comic_info.series,
+                            comic_info.title,
+                            comic_info.issue_num,
+                            comic_info.release_date,
+                            comic_info.release_day,
+                            comic_info.release_month,
+                            comic_info.release_year
                         FROM 
                             subcollections
                         INNER JOIN 
                             collection_refrence ON collection_refrence.collection_fk = subcollections.subcollect_fk 
                         INNER JOIN 
                             comic_ownership ON collection_refrence.copy_fk = comic_ownership.id
-                ) INTERSECT (
-                    (
-                        SELECT
-                            id
-                        FROM
-                            comic_ownership
-                        WHERE grade > 0
-                    ) UNION (
-                        SELECT
-                            copy_fk
-                        FROM
-                            signature_refrence
+                        INNER JOIN 
+                            comic_info ON comic_info.id = comic_ownership.comic_fk
+                    ) INTERSECT (
+                        (
+                            SELECT
+                                comic_ownership.id,
+                                comic_info.series,
+                                comic_info.title,
+                                comic_info.issue_num,
+                                comic_info.release_date,
+                                comic_info.release_day,
+                                comic_info.release_month,
+                                comic_info.release_year
+                            FROM
+                                comic_ownership
+                            INNER JOIN 
+                                comic_info ON comic_info.id = comic_ownership.comic_fk
+                            WHERE
+                                comic_ownership.grade > 0
+                        ) UNION (
+                            SELECT
+                                comic_ownership.id,
+                                comic_info.series,
+                                comic_info.title,
+                                comic_info.issue_num,
+                                comic_info.release_date,
+                                comic_info.release_day,
+                                comic_info.release_month,
+                                comic_info.release_year
+                            FROM
+                                signature_refrence
+                            INNER JOIN
+                                comic_ownership ON comic_ownership.id = signature_refrence.copy_fk
+                            INNER JOIN 
+                                comic_info ON comic_info.id = comic_ownership.comic_fk
+                        )
                     )
-                )
+                ) AS result
 
             """
         );
